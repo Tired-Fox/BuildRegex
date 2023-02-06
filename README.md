cf8f71ba9f1590d86631e800dc9c9ac784f78451
Zachary Boehm <zboehm104@gmail.com>
2023-02-05 21:51:18 -0600

    Set up basic idea for a regex builder

---
+++ README.md
@@ -0,0 +1,3 @@
+# Build Regex
+
+Class that allows you to build a regex string from understandably named methods. Allows for advanced regex operations and opens regex up for everyone without knowing the advanced parts of it. It will be treated similar to a query system or key word class builder syntax. Inspiration for this project is [magic-regexp](https://regexp.dev/getting-started/setup).
---
+++ build_regex/__init__.py
@@ -0,0 +1,38 @@
+"""
+builder().{method}()
+
+exactly
+and
+or
+digit
+character
+oneOrMore
+manyOrNone
+optional
+option
+not
+any
+whitespace
+htab
+carriage return
+line feed
+vtab
+form-feed
+[\b] backspace
+\0 NUL
+\xhh code hh / two hexadecimal digits
+\uhhhh UTF-16 code-unit / four hexadecimal digits
+\u{hhhh} or \u{hhhhh} character with the unicode value U+hhhh or U+hhhhh
+begin
+end
+word boundry
+look ahead
+look behind
+named captures
+not capture
+backreferences
+back reference named capture
+range
+UnicodePropertyValue | UnicodePropertyName=UnicodePropertyValue (\p)
+control character
+"""
